6ca6795624d8612fbf16bb52257ed57612dcdb60
Guang Liu <77652670+guangliu1755@users.noreply.github.com>
2021-11-06 17:31:17 -0700

    Add files via upload

---
+++ agitator_subsystem.cpp
@@ -0,0 +1,59 @@
+#include "agitator_subsystem.hpp"
+
+#include "tap/algorithms/math_user_utils.hpp"
+#include "tap/architecture/clock.hpp"
+
+namespace control
+{
+namespace agitator
+{
+void AgitatorSubsystem::refresh()
+{
+    if (isCalibrated)
+    {
+        actualAngle = getAngle();
+        /// \todo run pid
+
+        algorithms::EduPid::runControllerDerivateError((desiredAngle - actualAngle), (tap::arch::clock::getTimeMilliseconds() - prevTime)); 
+        prevTime = tap::arch::clock::getTimeMilliseconds(); 
+
+    }
+    else
+    {
+        calibrateHere();
+    }
+}
+
+bool AgitatorSubsystem::calibrateHere()
+{
+    if (!motor.isMotorOnline())
+    {
+        return false;
+    }
+    agitatorCalibratedZeroAngle = getUncalibratedAngle();
+    isCalibrated = true;
+    return true;
+}
+
+float AgitatorSubsystem::getAngle() const
+{
+    if (!isCalibrated)
+    {
+        return 0.0f;
+    }
+    return getUncalibratedAngle() - agitatorCalibratedZeroAngle;
+}
+
+float AgitatorSubsystem::getUncalibratedAngle() const
+{
+    // position is equal to the following equation:
+    // position = 2 * PI / encoder resolution * unwrapped encoder value / gear ratio
+    return (2.0f * M_PI / static_cast<float>(tap::motor::DjiMotor::ENC_RESOLUTION)) *
+           motor.getEncoderUnwrapped() / GEAR_RATIO;
+}
+
+void AgitatorSubsystem::setDesiredAngle(float newAngle) { desiredAngle = newAngle; }
+
+}  // namespace agitator
+
+}  // namespace control
---
+++ edu_pid.cpp
@@ -0,0 +1,31 @@
+#include "edu_pid.hpp"
+
+#include "tap/algorithms/math_user_utils.hpp"
+
+using tap::algorithms::limitVal;
+
+namespace algorithms
+{
+float EduPid::runControllerDerivateError(float error, float dt)
+{
+    currErrorP = error; 
+    
+    currErrorD = ((error - prevError) / dt);
+    prevError = error; 
+    
+    currErrorI = currErrorI + (error * dt); 
+    tap::algorithms::limitVal(currErrorI, -maxICumulative, maxICumulative); 
+    
+    float output = (kp * currErrorP) + (ki * currErrorI) + (kd * currErrorD); 
+    tap::algorithms::limitVal(output, -maxOutput, maxOutput); 
+    return output;
+}
+
+void EduPid::reset()
+{
+    currErrorP = 0.0f;
+    currErrorI = 0.0f;
+    currErrorD = 0.0f;
+    output = 0.0f;
+}
+}  // namespace algorithms
